ea03842a9a2d3bacf2f09a1ae540cf993b6ea1d4
Denis Kyznetsov <104186466+DenKuznets@users.noreply.github.com>
2023-12-12 16:51:15 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from './rooms/rooms.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, RoomsComponent, HomeComponent],
+  // templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   // inline template
-  // template: '<div>hello</div>',
-  // inline styles
-  // styles: [
-  //   `
-  //     div {
-  //       background-color: red;
-  //     }
-  //   `,
-  // ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
